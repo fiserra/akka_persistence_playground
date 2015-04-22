5a4e0957364e6f9b0555ed0577f833914010d7c8
Radu Fiser <radu.fiser@gmail.com>
2015-04-22 23:55:08 +0300

    finished with AtLeastOnceDeliveryExample

--- src/main/scala/com/fiser/akka/persistence/AtLeastOnceDeliveryExample.scala
+++ src/main/scala/com/fiser/akka/persistence/AtLeastOnceDeliveryExample.scala
@@ -1,9 +1,20 @@
 package com.fiser.akka.persistence
 
-import akka.actor.{Actor, ActorPath}
+import akka.actor._
 import akka.persistence.{AtLeastOnceDelivery, PersistentActor}
 
 object AtLeastOnceDeliveryExample extends App {
+
+  val system = ActorSystem("atLeastOnceDeliveryExample")
+  val destination = system.actorOf(Props(classOf[MyDestination]), "myDestination-4")
+  val persistentActor = system.actorOf(Props(classOf[MyPersistentActor], destination.path), "persistentActor-4")
+
+  persistentActor ! "a"
+  persistentActor ! "b"
+  persistentActor ! "c"
+
+  Thread.sleep(1000)
+  system.shutdown()
 
   case class Msg(deliveryId: Long, s: String)
 
@@ -15,28 +26,36 @@ object AtLeastOnceDeliveryExample extends App {
 
   case class MsgConfirmed(deliveryId: Long) extends Evt
 
-  class MyPersistentActor(destination: ActorPath) extends PersistentActor with AtLeastOnceDelivery {
-    override def receiveRecover: Receive = ???
-
-    override def receiveCommand: Receive = {
-      case s: String => persist(MsgSent(s))(updateState)
-      case Confirm(deliveryId) => persist(MsgConfirmed(deliveryId))(updateState)
+  class MyPersistentActor(destination: ActorPath) extends PersistentActor with AtLeastOnceDelivery with ActorLogging {
+    override def receiveRecover: Receive = {
+      case evt: Evt => updateState(evt)
     }
 
-    private def updateState(event: Evt): Unit = {
-      case MsgSent(s) =>
-        deliver(destination, deliveryId => Msg(deliveryId, s))
-      case MsgConfirmed(deliveryId) => confirmDelivery(deliveryId)
+    override def receiveCommand: Receive = {
+      case s: String =>
+        log.info("Received {}", s)
+        persist(MsgSent(s))(updateState)
+      case c@Confirm(deliveryId) =>
+        log.info("Received {} ", c)
+        log.info("Persisting {}", MsgConfirmed(deliveryId))
+        persist(MsgConfirmed(deliveryId))(updateState)
+    }
 
+    private def updateState(event: Evt): Unit = event match {
+      case msg@MsgSent(s) =>
+        log.info("Deliver {} to {} ", msg, destination)
+        deliver(destination, deliveryId => Msg(deliveryId, s))
+      case MsgConfirmed(deliveryId) =>
+        log.info("Delivery {} confirmed ", deliveryId)
+        confirmDelivery(deliveryId)
     }
   }
 
-  class MyDestination extends Actor {
+  class MyDestination extends Actor with ActorLogging {
     def receive = {
-      case Msg(deliveryId, s) =>
-        // ...
+      case msg@Msg(deliveryId, s) =>
+        log.info("Confirming receiving of {} ", msg)
         sender() ! Confirm(deliveryId)
     }
   }
-
 }
